5b14758ffa31ca283d1f147b3283ca5a27ca920c
Interstellar132 <50997498+Interstellar132@users.noreply.github.com>
2025-12-22 21:36:37 -0800

    Update intrglxy.js

--- intrglxy.js
+++ intrglxy.js
@@ -16,6 +16,31 @@ const client = new Client({
 const saveFile = './lastLevel.json';
 const checkInterval = 60 * 1000;
 
+const { clientId, guildId, token } = require('./config.json');
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+(async () => {
+  try {
+    console.log('Refreshing guild commands...');
+    await rest.put(
+      Routes.applicationGuildCommands(clientId, guildId),
+      { body: commands }
+    );
+    console.log('Guild commands refreshed!');
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
 // Create a collection for commands
 client.commands = new Collection();
 
