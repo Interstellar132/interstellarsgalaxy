2df891c3c94fcbed75a8e031d29ef469c9b49b9b
Interstellar132 <50997498+Interstellar132@users.noreply.github.com>
2025-12-25 19:15:26 -0800

    Update intrglxy.js

--- intrglxy.js
+++ intrglxy.js
@@ -10,6 +10,7 @@ const client = new Client({
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.GuildMessageReactions,
     GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildModeration
   ],
 });
 
