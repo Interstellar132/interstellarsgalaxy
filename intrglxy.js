ddd2c868c8833533d0f3dddb610bc8c6ccf7acdd
Interstellar132 <50997498+Interstellar132@users.noreply.github.com>
2025-12-25 20:10:56 -0800

    Update intrglxy.js

--- intrglxy.js
+++ intrglxy.js
@@ -222,21 +222,38 @@ async function dmOwner(client, message) {
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
+// Load commands
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
-  commands.push(command.data.toJSON());
+  if (command.data && typeof command.data.toJSON === 'function') {
+    commands.push(command.data.toJSON());
+  } else {
+    console.warn(`Command ${file} does not have valid SlashCommandBuilder data.`);
+  }
 }
 
-const rest = new REST({ version: '10' }).setToken(token);
-
+// Deploy commands
 (async () => {
+  const rest = new REST({ version: '10' }).setToken(token);
+
   try {
-    console.log('Refreshing guild commands...');
-    await rest.put(
-      Routes.applicationGuildCommands(clientId, guildId),
-      { body: commands }
-    );
-    console.log('Guild commands refreshed!');
+    console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+    // Use this for guild (testing) commands
+    if (guildId) {
+      await rest.put(
+        Routes.applicationGuildCommands(clientId, guildId),
+        { body: commands }
+      );
+      console.log(`Successfully reloaded commands for guild ${guildId}.`);
+    } else {
+      // Use this for global commands
+      await rest.put(
+        Routes.applicationCommands(clientId),
+        { body: commands }
+      );
+      console.log('Successfully reloaded global commands.');
+    }
   } catch (error) {
     console.error(error);
   }
