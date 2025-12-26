3862c64684e543c196ece24efdfce9005dc62e0a
Interstellar132 <50997498+Interstellar132@users.noreply.github.com>
2025-12-25 20:40:27 -0800

    Update permDiff.js

--- utils/permDiff.js
+++ utils/permDiff.js
@@ -1,3 +1,10 @@
+const { PermissionsBitField } = require('discord.js');
+
+/**
+ * Compare channel permission overwrites
+ * @param {Collection<Snowflake, PermissionOverwrites>} oldOverwrites 
+ * @param {Collection<Snowflake, PermissionOverwrites>} newOverwrites 
+ */
 function diffOverwrites(oldOverwrites, newOverwrites) {
   const changes = [];
 
@@ -15,12 +22,7 @@ function diffOverwrites(oldOverwrites, newOverwrites) {
     const addedDeny = newDeny.filter(p => !oldDeny.includes(p));
     const removedDeny = oldDeny.filter(p => !newDeny.includes(p));
 
-    if (
-      addedAllow.length ||
-      removedAllow.length ||
-      addedDeny.length ||
-      removedDeny.length
-    ) {
+    if (addedAllow.length || removedAllow.length || addedDeny.length || removedDeny.length) {
       changes.push({
         id,
         type: newOW.type, // role or member
@@ -35,4 +37,38 @@ function diffOverwrites(oldOverwrites, newOverwrites) {
   return changes;
 }
 
-module.exports = { diffOverwrites };
+/**
+ * Compare two role objects for property changes
+ * @param {Role} oldRole 
+ * @param {Role} newRole 
+ */
+function diffRole(oldRole, newRole) {
+  const changes = [];
+
+  if (oldRole.name !== newRole.name) {
+    changes.push({ field: 'Name', before: oldRole.name, after: newRole.name });
+  }
+  if (oldRole.color !== newRole.color) {
+    changes.push({ field: 'Color', before: `#${oldRole.color.toString(16).padStart(6, '0')}`, after: `#${newRole.color.toString(16).padStart(6, '0')}` });
+  }
+  if (oldRole.hoist !== newRole.hoist) {
+    changes.push({ field: 'Hoist', before: oldRole.hoist, after: newRole.hoist });
+  }
+  if (oldRole.mentionable !== newRole.mentionable) {
+    changes.push({ field: 'Mentionable', before: oldRole.mentionable, after: newRole.mentionable });
+  }
+
+  // Permissions diff
+  const oldPerms = new PermissionsBitField(oldRole.permissions);
+  const newPerms = new PermissionsBitField(newRole.permissions);
+  const added = newPerms.missing(oldPerms);
+  const removed = oldPerms.missing(newPerms);
+
+  if (added.length || removed.length) {
+    changes.push({ field: 'Permissions', added, removed });
+  }
+
+  return changes;
+}
+
+module.exports = { diffOverwrites, diffRole };
