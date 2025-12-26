c8c6b1527ef7d4394571b4a7f9c15cd61785750f
Interstellar132 <50997498+Interstellar132@users.noreply.github.com>
2025-12-25 19:59:12 -0800

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -1,69 +1,27 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
-
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('warn')
-    .setDescription('Issue warning')
-    .addUserOption(option =>
-      option
-        .setName('user')
-        .setDescription('User to warn')
-        .setRequired(true)
-    )
-    .addStringOption(option =>
-      option
-        .setName('reason')
-        .setDescription('Reason for the warning')
-        .setRequired(false)
-    )
-    // Admin-only
-    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+  data: {
+    name: 'warn',
+    description: 'Warn a member'
+  },
+  async execute(interaction) { // <-- async here
+    const user = interaction.options.getUser('user');
+    const reason = interaction.options.getString('reason') || 'No reason provided';
 
-  async execute(interaction) {
-    const target = interaction.options.getUser('user');
-    const reason =
-      interaction.options.getString('reason') || 'No reason provided';
+    // DM the user
+    await user.send(`You were warned for: ${reason}`).catch(() => null);
 
-    // Make sure the user is still in the guild
-    const member = await interaction.guild.members
-      .fetch(target.id)
-      .catch(() => null);
-
-    if (!member) {
-      return interaction.reply({
-        content: 'I cannot find this user.',
-        ephemeral: true
-      });
-    }
-
-    // DM the warned user
-    let dmSuccess = true;
-    try {
-      await target.send(
-        `You have been warned for ${reason}. Please refer to the rules channel of the server.`
-      );
-    } catch (err) {
-      dmSuccess = false;
-    }
-
-    // Confirm to moderator
-    await interaction.reply({
-      content:
-        `**${target.tag}** has been warned.` +
-        (dmSuccess ? '' : '\n Could not DM the user.'),
-      ephemeral: false
+    // Log the warning
+    await sendLog(interaction.client, {
+      title: '⚠️ Member Warned',
+      color: 0xED4245,
+      fields: [
+        { name: 'User', value: user.tag },
+        { name: 'Reason', value: reason },
+        { name: 'Moderator', value: interaction.user.tag }
+      ],
+      timestamp: new Date()
     });
+
+    await interaction.reply({ content: `Warned ${user.tag}`, ephemeral: true });
   }
 };
-
-const { sendLog } = require('../utils/logger');
-
-await sendLog(interaction.client, {
-  title: '⚠️ Member Warned',
-  color: 0xFEE75C,
-  fields: [
-    { name: 'User', value: target.tag },
-    { name: 'Moderator', value: interaction.user.tag },
-    { name: 'Reason', value: reason }
-  ]
-});
