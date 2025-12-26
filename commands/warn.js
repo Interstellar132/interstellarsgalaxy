1f2466343ce66fa18de61102b71c0b313adc10b7
Interstellar132 <50997498+Interstellar132@users.noreply.github.com>
2025-12-25 18:52:07 -0800

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -1,1 +1,58 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('warn')
+    .setDescription('Issue warning')
+    .addUserOption(option =>
+      option
+        .setName('user')
+        .setDescription('User to warn')
+        .setRequired(true)
+    )
+    .addStringOption(option =>
+      option
+        .setName('reason')
+        .setDescription('Reason for the warning')
+        .setRequired(false)
+    )
+    // Admin-only
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+
+  async execute(interaction) {
+    const target = interaction.options.getUser('user');
+    const reason =
+      interaction.options.getString('reason') || 'No reason provided';
+
+    // Make sure the user is still in the guild
+    const member = await interaction.guild.members
+      .fetch(target.id)
+      .catch(() => null);
+
+    if (!member) {
+      return interaction.reply({
+        content: 'I cannot find this user.',
+        ephemeral: true
+      });
+    }
+
+    // DM the warned user
+    let dmSuccess = true;
+    try {
+      await target.send(
+        `You have been warned for ${reason}. Please refer to the rules channel of the server.`
+      );
+    } catch (err) {
+      dmSuccess = false;
+    }
+
+    // Confirm to moderator
+    await interaction.reply({
+      content:
+        `**${target.tag}** has been warned.` +
+        (dmSuccess ? '' : '\n Could not DM the user.'),
+      ephemeral: false
+    });
+  }
+};
 
