c827872c0ab49901a2e6756481dd220b63e3f4d8
Interstellar132 <50997498+Interstellar132@users.noreply.github.com>
2025-12-25 20:05:11 -0800

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -1,16 +1,27 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { sendLog } = require('../utils/logger.js');
+
 module.exports = {
-  data: {
-    name: 'warn',
-    description: 'Warn a member'
-  },
-  async execute(interaction) { // <-- async here
+  data: new SlashCommandBuilder()
+    .setName('warn')
+    .setDescription('Warn a member')
+    .addUserOption(option =>
+      option.setName('user')
+        .setDescription('User to warn')
+        .setRequired(true))
+    .addStringOption(option =>
+      option.setName('reason')
+        .setDescription('Reason for warning')
+        .setRequired(false)),
+  
+  async execute(interaction) {
     const user = interaction.options.getUser('user');
     const reason = interaction.options.getString('reason') || 'No reason provided';
 
     // DM the user
     await user.send(`You were warned for: ${reason}`).catch(() => null);
 
-    // Log the warning
+    // Log warning
     await sendLog(interaction.client, {
       title: '⚠️ Member Warned',
       color: 0xED4245,
