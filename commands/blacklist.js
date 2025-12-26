dc833aa20f2989c7a25fe760aade013d73141292
Interstellar132 <50997498+Interstellar132@users.noreply.github.com>
2025-12-25 21:15:05 -0800

    Update blacklist.js

--- commands/blacklist.js
+++ commands/blacklist.js
@@ -1,1 +1,36 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('blockword')
+    .setDescription('Add or remove a word from the automod blacklist')
+    .addStringOption(option =>
+      option.setName('word')
+        .setDescription('Word to block/unblock')
+        .setRequired(true))
+    .addStringOption(option =>
+      option.setName('action')
+        .setDescription('Add or remove the word')
+        .setRequired(true)
+        .addChoices(
+          { name: 'Add', value: 'add' },
+          { name: 'Remove', value: 'remove' }
+        )),
+  async execute(interaction) {
+    const word = interaction.options.getString('word').toLowerCase();
+    const action = interaction.options.getString('action');
+    const guildId = interaction.guild.id;
+
+    if (!wordBlacklist.has(guildId)) wordBlacklist.set(guildId, new Set());
+    const set = wordBlacklist.get(guildId);
+
+    if (action === 'add') {
+      set.add(word);
+      await interaction.reply({ content: `✅ Word **${word}** added to blacklist.`, ephemeral: true });
+    } else if (action === 'remove') {
+      set.delete(word);
+      await interaction.reply({ content: `✅ Word **${word}** removed from blacklist.`, ephemeral: true });
+    }
+  }
+};
 
