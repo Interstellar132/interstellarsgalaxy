8c83affe5ccacaeb48b94581a0576dfdbdaa7cbd
Interstellar132 <50997498+Interstellar132@users.noreply.github.com>
2025-12-27 22:56:12 -0800

    Update automod.js

--- utils/automod.js
+++ utils/automod.js
@@ -1,6 +1,5 @@
 const warnings = new Map();
-const wordBlacklist = new Map();
 const userSpam = new Map();
 
-module.exports = { warnings, wordBlacklist, userSpam };
+module.exports = { warnings, userSpam };
 
