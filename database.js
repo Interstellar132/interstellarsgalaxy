3918f94064631c40b4a818a8696414e9f0316491
Interstellar132 <50997498+Interstellar132@users.noreply.github.com>
2026-01-09 07:28:41 -0800

    Create database.js

---
+++ database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+modulue.exports = async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('I have connected to the database');
+  } catch (err) {
+    console.error('I did not want to connect', err);
+    process.exit(1);
+  }
+};
