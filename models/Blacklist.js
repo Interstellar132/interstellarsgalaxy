2764c0ac69150ae79a131d987698418661c4a2be
Interstellar132 <50997498+Interstellar132@users.noreply.github.com>
2026-01-09 08:18:42 -0800

    Update Blacklist.js

--- models/Blacklist.js
+++ models/Blacklist.js
@@ -1,9 +1,27 @@
 const { Schema, model } = require('mongoose');
 
 const blacklistSchema = new Schema({
-  guildId: { type: String, required: true, index: true },
-  word: { type: String, required: true },
-  regex: { type: String, required: true } // stored as string
+  guildId: {
+    type: String,
+    required: true,
+    index: true
+  },
+  word: {
+    type: String,
+    required: true
+  },
+  regex: {
+    type: String,
+    required: true
+  },
+  addedBy: {
+    type: String,
+    required: true
+  },
+  addedAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 blacklistSchema.index({ guildId: 1, word: 1 }, { unique: true });
