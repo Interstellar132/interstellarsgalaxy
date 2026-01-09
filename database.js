7a00c3e3e89c085a72c08820f5122ba6feddc198
Interstellar132 <50997498+Interstellar132@users.noreply.github.com>
2026-01-09 07:47:26 -0800

    Update database.js

--- database.js
+++ database.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 modulue.exports = async function connectDB() {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      autoIndex: true
+    });
     console.log('I have connected to the database');
   } catch (err) {
     console.error('I did not want to connect', err);
