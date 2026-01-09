ccd669aa77b08a91b7081c60929068d0410674a7
Interstellar132 <50997498+Interstellar132@users.noreply.github.com>
2026-01-09 08:47:31 -0800

    Update warnings.js

--- utils/warnings.js
+++ utils/warnings.js
@@ -1,32 +1,53 @@
 const Warning = require('../models/Warning');
 
-module.exports = {
-  async get(guildId, userId) {
-    const doc = await Warning.findOne({ guildId, userId });
-    return doc ? doc.count : 0;
-  },
+const DAY = 24 * 60 * 60 * 1000;
 
+function applyDecay(doc) {
+  if (!doc) return 0;
+
+  const daysPassed = Math.floor(
+    (Date.now() - doc.lastUpdated.getTime()) / DAY
+  );
+
+  if (daysPassed > 0) {
+    doc.count = Math.max(0, doc.count - daysPassed);
+    doc.lastUpdated = new Date();
+  }
+
+  return doc.count;
+}
+
+module.exports = {
   async increment(guildId, userId) {
-    const doc = await Warning.findOneAndUpdate(
-      { guildId, userId },
-      {
-        $inc: { count: 1 },
-        $set: { updatedAt: new Date() }
-      },
-      { upsert: true, new: true }
-    );
+    let doc = await Warning.findOne({ guildId, userId });
+
+    if (!doc) {
+      doc = await Warning.create({
+        guildId,
+        userId,
+        count: 1
+      });
+      return 1;
+    }
+
+    applyDecay(doc);
+    doc.count += 1;
+    doc.lastUpdated = new Date();
+    await doc.save();
 
     return doc.count;
   },
 
-  async reset(guildId, userId) {
-    await Warning.findOneAndUpdate(
-      { guildId, userId },
-      { count: 0 }
-    );
+  async get(guildId, userId) {
+    const doc = await Warning.findOne({ guildId, userId });
+    if (!doc) return 0;
+
+    applyDecay(doc);
+    await doc.save();
+    return doc.count;
   },
 
-  async clearGuild(guildId) {
-    await Warning.deleteMany({ guildId });
+  async reset(guildId, userId) {
+    await Warning.deleteOne({ guildId, userId });
   }
 };
