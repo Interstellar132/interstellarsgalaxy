1bb232f7672f7b74b2c2e642fdd30b6615b82a61
Interstellar132 <50997498+Interstellar132@users.noreply.github.com>
2026-09-16 08:37:42 -0700

    Update blacklist.js

--- commands/blacklist.js
+++ commands/blacklist.js
@@ -5,6 +5,7 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName('blacklist')
     .setDescription('Prohibited terms management')
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
     .addSubcommand(subcommand =>
       subcommand
         .setName('add')
